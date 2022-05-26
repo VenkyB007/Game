a6c26243b3ba5a77f0ebe52234230782437591b4
VenkyB007 <102458222+VenkyB007@users.noreply.github.com>
2022-05-26 16:10:46 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Data struct {
+	Name      string      `json:"name"`
+	Character []Character `json:"character"`
+}
+
+type Character struct {
+	Name      string `json:"name"`
+	Max_Power int    `json:"max_power"`
+}
+
+func main() {
+	r := gin.Default()
+	var data []Data
+	go r.GET("/avengers", func(c *gin.Context) {
+
+		var avengers Data
+
+		resp, _ := http.Get("http://www.mocky.io/v2/5ecfd5dc3200006200e3d64b")
+		body, _ := ioutil.ReadAll(resp.Body)
+		err := json.Unmarshal(body, &avengers)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		data = append(data, avengers)
+		c.JSON(http.StatusOK, avengers)
+	})
+
+	go r.GET("/anti-heros", func(c *gin.Context) {
+
+		var anti_hero Data
+
+		resp, _ := http.Get("http://www.mocky.io/v2/5ecfd630320000f1aee3d64d")
+		body, _ := ioutil.ReadAll(resp.Body)
+		err := json.Unmarshal(body, &anti_hero)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		data = append(data, anti_hero)
+		c.JSON(http.StatusOK, anti_hero)
+	})
+
+	go r.GET("/mutants", func(c *gin.Context) {
+
+		var mutants Data
+
+		resp, _ := http.Get("http://www.mocky.io/v2/5ecfd6473200009dc1e3d64e")
+		body, _ := ioutil.ReadAll(resp.Body)
+		err := json.Unmarshal(body, &mutants)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		data = append(data, mutants)
+		c.JSON(http.StatusOK, mutants)
+	})
+	go r.GET("/get-all", func(c *gin.Context) {
+		c.JSON(http.StatusOK, data)
+
+	})
+
+	r.Run(":9009")
+}
